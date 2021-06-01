c838b8957206285f812ed436fac5bb38c07f828d
guyfromthesky <giang.vutruong@outlook.com>
2021-06-01 23:21:12 +0700

    remove files

--- test/blob download test.py
+++ test/blob download test.py
@@ -14,15 +14,20 @@ import pickle
 AppConfig = ConfigLoader()
 Configuration = AppConfig.Config
 AppLanguage  = Configuration['Document_Translator']['app_lang']
-tm = Configuration['TranslationMemory']['path']
-print(Configuration['License_File']['path'])
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = Configuration['License_File']['path']
+tm = Configuration['translation_memory']['path']
+print(Configuration['license_file']['path'])
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = Configuration['license_file']['path']
 
-GlossaryID = Configuration['Glossary_ID']['value']
+GlossaryID = Configuration['glossary_id']['value']
 print(tm)
 
-MyTranslator = Translator(From_Language = 'ko', To_Language = 'en', GlossaryID =  GlossaryID, ProactiveTMTranslate= True, TM_Path=tm)
+MyTranslator = Translator()
 
+#download_blob(self, bucket_id, blob_id, download_path):
+file = 'C:\\Users\\evan\\Documents\\GitHub\\aiotranslator4\\test files\\V4.csv'
+d_file = 'C:\\Users\\evan\\Documents\\GitHub\\aiotranslator4\\test files\\V4_test.csv'
+MyTranslator.upload_blob(blob_id = 'test/test.csv', upload_path = file)
 
+MyTranslator.download_blob(blob_id = 'test/test.csv', download_path = d_file)
 
 
