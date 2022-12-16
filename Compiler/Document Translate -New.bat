7a67e1f8f666e88fbed954b8f4d6a328d1d7dada
guyfromthesky <giang.vutruong@outlook.com>
2022-12-16 09:24:29 +0700

    quick update

---
+++ Compiler/Document Translate -New.bat
@@ -0,0 +1,2 @@
+"D:\All env\Translator Env\Scripts\pyinstaller.exe" "C:\Users\evan\Documents\GitHub\aiotranslator4\Compiler\Document Translator - New.spec"
+cmd /k
--- Compiler/Document Translate.bat
+++ Compiler/Document Translate.bat
@@ -1,2 +1,2 @@
-"D:\All env\Translator Env\Scripts\pyinstaller.exe" "..\Document Translator.py" -F -w --additional-hooks-dir="..\hooks" --clean --icon="..\icon\Document Translator.ico"
+"D:\All env\Translator Env\Scripts\pyinstaller.exe" "..\Document Translator.py" --additional-hooks-dir="..\hooks" -F -w --clean --icon="..\icon\Document Translator.ico"
 cmd /k
